c6dc2b715d2d5e1ed1f2642491ec6c48ec0262dd
Brent Collier <brentmc79@gmail.com>
2014-06-21 13:58:30 -0700

    Don't call parseFloat from the Number context

--- js/stockstatus.js
+++ js/stockstatus.js
@@ -30,7 +30,7 @@ function StockStatus(symbols) {
           quote: quote.AskRealtime,
           index: index.toString(),
           daysHigh: quote.DaysHigh,
-          daysLow: Number.parseFloat(quote.DaysLow).toString(),
+          daysLow: parseFloat(quote.DaysLow).toString(),
           change: quote.Change,
           changeColor: quote.Change.charAt(0) == '+' ? 'green' : 'red'
         }
